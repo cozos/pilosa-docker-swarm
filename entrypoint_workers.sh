20ca98e194361b5d97ff6cf4f61c3300305af8ee
Arwin Tio <Arwin.tio@adroll.com>
2020-12-19 00:36:39 -0800

    Fix gossip seeds

--- entrypoint_workers.sh
+++ entrypoint_workers.sh
@@ -5,17 +5,17 @@ apk add bind-tools
 
 MY_HOSTNAME=`hostname -i`
 
-for NODE in $(dig +short pilosa-workers); do
-    if [[ $NODE != $MY_HOSTNAME ]]; then
-      OTHER_WORKER_HOSTNAME=$NODE
-      break
-    fi
-done
+# for NODE in $(dig +short pilosa-workers); do
+#     if [[ $NODE != $MY_HOSTNAME ]]; then
+#       OTHER_WORKER_HOSTNAME=$NODE
+#       break
+#     fi
+# done
 
 ./pilosa server \
     --cluster.coordinator=true \
     --bind=0.0.0.0:10101 \
-    --gossip.seeds=pilosa-leader:11101,$OTHER_WORKER_HOSTNAME:11101 \
+    --gossip.seeds=pilosa-leader:11101 \
     --gossip.advertise-host=$MY_HOSTNAME \
     --gossip.advertise-port=15001 \
     --gossip.port=11101 \
